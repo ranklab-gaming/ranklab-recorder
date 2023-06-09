dd0e16cd904b9b3cfbed211e0fd3e95b0ef6d66b
Eugenio Depalo <eugeniodepalo@gmail.com>
2023-06-09 16:54:51 +0000

    Formatting

--- log.py
+++ log.py
@@ -5,12 +5,12 @@ import logging
 def setup_logger():
     logger = logging.getLogger()
     handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-    log_level = os.getenv('LOG_LEVEL', 'INFO')
-    if log_level in ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']:
+    log_level = os.getenv("LOG_LEVEL", "INFO")
+    if log_level in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]:
         logger.setLevel(log_level)
     else:
         logger.setLevel(logging.INFO)
